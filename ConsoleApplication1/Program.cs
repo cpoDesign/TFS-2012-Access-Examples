f155ee23efce9fc6ec08272c71c625463d88ab19
pav <pvl.svarc@gmail.com>
2015-05-22 13:46:46 +0100

    Example to access change sets associated with work items

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TFS2012.Access.ChangeSets;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var workItems = new List<int>()
+            {
+                27400,
+                27389,
+                31417,
+                27439
+            };
+            
+            Uri tfsUri = new Uri("http://tfs:8080/tfs/DefaultCollection");
+
+            var list = ChangeSets.QueryTfsForChangeSets(tfsUri, workItems); 
+
+            // dump results into file
+            var file = System.IO.File.CreateText("output.csv");
+
+            foreach (var changeSet in list.OrderBy(x => x.ChangesetId))
+            {
+                string changeSetRecord = string.Format("{0}, Committer,{2},Comment,{1},",
+                    changeSet.ChangesetId,
+                    changeSet.Comment,
+                    changeSet.CommitterDisplayName);
+
+                file.WriteLine(changeSetRecord);
+            }
+
+            file.Flush();
+        }
+    }
+}
---
+++ TFS2012.ChangeSets.Query/ChangeSets.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.TeamFoundation.Client;
+using Microsoft.TeamFoundation.VersionControl.Client;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace TFS2012.ChangeSets.Query
+{
+    public class ChangeSets
+    {
+        public static List<Changeset> QueryTfsForChangeSets(Uri tfsUri, List<int> workItems)
+        {
+            var list = new List<Changeset>();
+            using (var projectCollection = new TfsTeamProjectCollection(tfsUri, new UICredentialsProvider()))
+            {
+                projectCollection.EnsureAuthenticated();
+                var workItemStore = projectCollection.GetService<WorkItemStore>();
+                var versionControlServer = projectCollection.GetService<VersionControlServer>();
+                var artifactProvider = versionControlServer.ArtifactProvider;
+                string queryString = string.Format("Select [State], [Title] From WorkItems Where ([System.Id] In ({0}))", string.Join(",", workItems));
+
+                // Set up a dictionary to pass "User Story" as the value of the type variable.
+                Dictionary<string, string> variables = new Dictionary<string, string>();
+
+                // Create and run the query.
+                var query = new Query(workItemStore, queryString, variables);
+
+                WorkItemCollection results = query.RunQuery();
+
+                foreach (WorkItem workItem in results)
+                {
+                    //Console.WriteLine("WI: {0}, Title: {1}", workItem.Id, workItem.Title);
+                    foreach (Changeset changeset in workItem.Links.OfType<ExternalLink>()
+                        .Select(link => artifactProvider
+                            .GetChangeset(new Uri(link.LinkedArtifactUri)))
+                        .OrderBy(x => x.ChangesetId))
+                    {
+                        list.Add(changeset);
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+}
